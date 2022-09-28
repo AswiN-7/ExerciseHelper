ada25a421a3dff4e130916a96bc7635a9eaa1699
AswiN <aswin18027@cse.ssn.edu.in>
2022-09-28 20:59:56 +0530

    do and rest phase 1 finished

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -6,89 +6,224 @@ modeButton.innerText = "Light";
 
 modeButton.addEventListener('click', toggleMode);
 
-function toggleMode(){
+function toggleMode() {
     document.body.classList.toggle('dark-mode');
-    if(modeButton.innerText == 'Light'){
+    if (modeButton.innerText == 'Light') {
         modeButton.innerText = 'Dark';
     }
-    else{
+    else {
         modeButton.innerText = 'Light';
 
     }
 }
+// ====================> Screen lock <===============================
+let screenLock = null;
 
-// ====================> cat n cow <===============================
-var catRoundsElement = document.getElementById('cat-rounds');
-var catTimeElement = document.getElementById('cat-time');
+function isScreenLockSupported() {
+    return ('wakeLock' in navigator);
+}
+async function getScreenLock() {
+    if (isScreenLockSupported()) {
+        let screenLock;
+        try {
+            screenLock = await navigator.wakeLock.request('screen');
+        } catch (err) {
+            console.log(err.name, err.message);
+        }
+        return screenLock;
+    }
+}
 
-var catCalculateButton = document.getElementById('cat-calculate-time');
-var catRequiredTimeDisplay = document.getElementById('cat-required-time');
-var catStartButton = document.getElementById('cat-start-exercise');
+function release() {
+    if (typeof screenLock !== "undefined" && screenLock != null) {
+        screenLock.release()
+            .then(() => {
+                console.log("Lock released 🎈");
+                screenLock = null;
+            });
+    }
+}
 
-var catRoundsDisplay = document.getElementById('cat-round-diaplay');
-var catTimeDisplay = document.getElementById('cat-display-time');
-var catDirectionDisplay = document.getElementById('cat-direction');
 
-var catTotalSeconds;
-var catRounds;
-var catTime;
+// ====================> dodo <===============================
+var DoDoRoundsElement = document.getElementById('dodo-rounds');
+var dodoTimeElement = document.getElementById('dodo-time');
 
-function catCalculateTime(){
-    console.log(catRoundsElement.value);
-    catRounds = parseInt(catRoundsElement.value);
-    catTime = parseInt(catTimeElement.value);
-    if(isNaN(catRounds) || isNaN(catTime)){
+var dodoCalculateButton = document.getElementById('dodo-calculate-time');
+var dodoRequiredTimeDisplay = document.getElementById('dodo-required-time');
+var DoDOStartButton = document.getElementById('dodo-start-exercise');
+var dodoStopButton = document.getElementById('dodo-stop');
+
+var DoDoRoundsDisplay = document.getElementById('dodo-round-diaplay');
+var dodoTimeDisplay = document.getElementById('dodo-display-time');
+var dodoDirectionDisplay = document.getElementById('dodo-direction');
+
+var dodoTotalSeconds;
+var DoDoRounds;
+var dodoTime;
+
+var Doing = 0;
+var Stop = 0;
+
+function dodoCalculateTime() {
+    console.log(DoDoRoundsElement.value);
+    DoDoRounds = parseInt(DoDoRoundsElement.value);
+    dodoTime = parseInt(dodoTimeElement.value);
+    if (isNaN(DoDoRounds) || isNaN(dodoTime)) {
         console.log("not na number");
         alert("enter number")
     }
-    totalSeconds = 2*catRounds*catTime;
-    minutes = Math.floor(totalSeconds/60);
-    seconds = totalSeconds%60;
+    totalSeconds = 2 * DoDoRounds * dodoTime;
+    minutes = Math.floor(totalSeconds / 60);
+    seconds = totalSeconds % 60;
     // console.log();
-    catRequiredTimeDisplay.innerText = " 0" + minutes + " : " + seconds;
+    dodoRequiredTimeDisplay.innerText = " 0" + minutes + " : " + seconds;
 }
 
-catCalculateButton.addEventListener('click', catCalculateTime);
+dodoCalculateButton.addEventListener('click', dodoCalculateTime);
 
 
-function wait(s){
-    start = Date.now();
-    now = start;
-    while (now <= start + s*1000){
-        now = Date.now();
+
+// function wait(s) {
+//     start = Date.now();
+//     now = start;
+//     while (now <= start + s * 1000) {
+//         now = Date.now();
+//     }
+// }
+
+async function startdodo() {
+    if (Doing == 0) {
+        screenLock = await getScreenLock();
+        Doing = 1;
+        Stop = 0;
+        for (let currentRound = 1; currentRound <= DoDoRounds; currentRound++) {
+            DoDoRoundsDisplay.innerText = currentRound;
+            for (let s = 1; s <= dodoTime; s++) {
+                dodoTimeDisplay.innerText = s;
+                // wait(1);
+                if(Stop == 1){
+                    Doing=0;
+                    return
+                }
+                await new Promise(done => setTimeout(() => done(), 1000));
+            }
+            play();
+            dodoDirectionDisplay.classList.toggle('rotate__180');
+            // play sound
+            console.log("play sound");
+            for (let s = 1; s <= dodoTime; s++) {
+                // wait(1);
+                if(Stop == 1){
+                    Doing=0;
+                    return
+                }
+                dodoTimeDisplay.innerText = s;
+                await new Promise(done => setTimeout(() => done(), 1000));
+            }
+            play();
+            // play sound
+            console.log("play sound");
+            dodoDirectionDisplay.classList.toggle('rotate__180');
+            Doing = 0;
+            release();
+        }
     }
 }
 
-async function startCat() {
-    for (let currentRound = 1; currentRound <= catRounds; currentRound++) {
-        catRoundsDisplay.innerText = currentRound;
-        for (let s = 1; s <= catTime; s++) {
-            catTimeDisplay.innerText = s;
-            // wait(1);
-            await new Promise(done => setTimeout(() => done(), 1000));
-        }
-        catDirectionDisplay.classList.toggle('rotate__180');
-        play();
-        // play sound
-        console.log("play sound");
-        for (let s = 1; s <= catTime; s++) {
-            // wait(1);
-            catTimeDisplay.innerText = s;
-            await new Promise(done => setTimeout(() => done(), 1000));
-        }
-        play();
-        // play sound
-        console.log("play sound");
-        catDirectionDisplay.classList.toggle('rotate__180');
 
-
-    }  
-}
 function play() {
     audioUrl = String.raw`assets\audio\pinwheel.mp3`
     var audio = new Audio(audioUrl);
-      audio.play();
-    }
-catStartButton.addEventListener('click', startCat)
+    audio.play();
+}
+
+DoDOStartButton.addEventListener('click', startdodo)
+
+dodoStopButton.addEventListener('click', ()=>{
+    Stop = 1;
+
+})
 console.log("end");
 
+// ====================> dorest <===============================
+var doRestRoundsElement = document.getElementById('do-rest-rounds');
+var doRestDoTimeElement = document.getElementById('dorest-do-time');
+var doRestRestTimeElement = document.getElementById('dorest-rest-time');
+
+var doRestCalculateButton = document.getElementById('dorest-calculate-time');
+var doRestRequiredTimeDisplay = document.getElementById('dorest-required-time');
+var doRestStartButton = document.getElementById('dorest-start-exercise');
+var doRestStopButton = document.getElementById('dorest-stop');
+
+var doRestRoundsDisplay = document.getElementById('dorest-round-diaplay');
+var doRestTimeDisplay = document.getElementById('dorest-display-time');
+var doRestDirectionDisplay = document.getElementById('dorest-direction');
+
+var doRestTotalSeconds;
+var doRestRounds;
+var doRestDo;
+var doRestRest;
+
+function doRestCalculateTime() {
+    console.log(doRestRoundsElement.value);
+    doRestRounds = parseInt(doRestRoundsElement.value);
+    doRestDo = parseInt(doRestDoTimeElement.value);
+    doRestRest = parseInt(doRestRestTimeElement.value);
+    if (isNaN(doRestRounds) || isNaN(doRestDo)|| isNaN(doRestRest)) {
+        console.log("not na number");
+        alert("enter number")
+    }
+    totalSeconds = doRestRounds * (doRestDo + doRestRest);
+    minutes = Math.floor(totalSeconds / 60);
+    seconds = totalSeconds % 60;
+    // console.log();
+    doRestRequiredTimeDisplay.innerText = " 0" + minutes + " : " + seconds;
+}
+
+doRestCalculateButton.addEventListener('click', doRestCalculateTime);
+
+async function startDoRest() {
+    if (Doing == 0) {
+        screenLock = await getScreenLock();
+        Doing = 1;
+        Stop = 0;
+        for (let currentRound = 1; currentRound <= doRestRounds; currentRound++) {
+            doRestRoundsDisplay.innerText = currentRound;
+            for (let s = 1; s <= doRestDo; s++) {
+                doRestTimeDisplay.innerText = s;
+                // wait(1);
+                if(Stop == 1){
+                    Doing=0;
+                    return
+                }
+                await new Promise(done => setTimeout(() => done(), 1000));
+            }
+            play();
+            doRestDirectionDisplay.classList.toggle('rotate__90');
+            // play sound
+            console.log("play sound");
+            for (let s = 1; s <= doRestRest; s++) {
+                // wait(1);
+                if(Stop == 1){
+                    Doing=0;
+                    return
+                }
+                doRestTimeDisplay.innerText = s;
+                await new Promise(done => setTimeout(() => done(), 1000));
+            }
+            play();
+            // play sound
+            console.log("play sound");
+            doRestDirectionDisplay.classList.toggle('rotate__90');
+            Doing = 0;
+            release();
+        }
+    }
+}
+
+doRestStartButton.addEventListener('click', startDoRest)
+
+doRestStopButton.addEventListener('click', ()=>{
+    Stop = 1;
+})
